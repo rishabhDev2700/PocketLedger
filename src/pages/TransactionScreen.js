eb0d43c1dc66853efedb250166546d8b8b647688
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-01-29 01:07:11 +0530

    Fixed transaction date and useEffect callback

--- src/pages/TransactionScreen.js
+++ src/pages/TransactionScreen.js
@@ -1,57 +1,53 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { TransactionsLayout } from '../components/TransactionLayout';
-import { InputField, InputButton, Select, AddButton, InputWrapper, Wrapper, ListWrapper } from '../components/InputField';
-import { Transaction } from 'firebase/firestore';
-import { addDoc, collection, Timestamp, query, where, getDocs, orderBy } from "firebase/firestore";
-import { db } from '../FirebaseConfig';
-import { AuthContext, UserContext } from '../App';
 import { useNavigate } from 'react-router-dom';
+import { addDoc, collection, Timestamp, query, where, orderBy, getDocs } from "firebase/firestore";
+import { TransactionsLayout } from '../components/TransactionLayout';
+import { Transaction } from './../components/Transaction'
+import { InputField, InputButton, Select, AddButton, InputWrapper, Wrapper, ListWrapper } from '../components/InputField';
+import { AuthContext, UserContext } from '../App';
+import { db } from '../FirebaseConfig';
+
+
 export const TransactionScreen = () => {
     const [transactionType, setTransactionType] = useState(true);
     const [amount, setAmount] = useState(0);
     const [amountFor, setAmountFor] = useState('food');
     const [list, setList] = useState([]);
+    const [item, setItem] = useState({});
     const userContext = useContext(UserContext);
     const authContext = useContext(AuthContext);
     const navigate = useNavigate();
-    useEffect(() => {
-        if (!authContext.get&&userContext.user!==null) {
-            navigate('/');
-        }
-        const document = collection(db, 'transactions');
-        const q = query(document, where('user', '==', userContext.user.uid), orderBy("date", 'asc'));
-        async function fetchDocs(q) {
-            return await getDocs(q);
-        }
-        const result = fetchDocs(q);
+
+    const document = collection(db, 'transactions');
+    const q = query(document, where('user', '==', userContext.user.uid), orderBy("date", 'asc'));
+    const fetchDocs = async (q, setList) => {
         const docList = [];
+        const result = await getDocs(q);
         result.forEach((doc) => {
-            docList.push((<><Transaction key={doc.data().id}>{doc.data().amount}--{doc.data().for}---{doc.data().date.toLocaleString()}</Transaction><hr /></>));
+            const date =  doc.data().date.toDate();
+            docList.push(<>
+                <Transaction key={doc.id.toString()}>{doc.data().amount}=={doc.data().for}=={date.toDateString()}</Transaction><hr />
+            </>)
         });
         setList(docList);
-    }, [list, authContext.get, navigate]);
-    const handleType = () => {
-        setTransactionType(!transactionType);
     }
-    const handleAmount = (e) => {
-
-        setAmount(e.target.value);
-    }
-    const handleFor = (e) => {
-        setAmountFor(e.target.value);
-    }
+    useEffect(() => {
+        if (!authContext.get) {
+            navigate('/');
+        }
+        fetchDocs(q, setList);
+    }, [authContext.get, navigate, item]);
     const handleTransaction = async () => {
         const uid = userContext.user.uid;
         const values = { type: transactionType, amount: amount, for: amountFor, user: uid, date: Timestamp.now().toDate() };
         try {
             const document = collection(db, 'transactions');
             await addDoc(document, values);
-            console.log('Saved transaction');
+            setItem(values);
         }
         catch (e) {
             console.log('Error occurred while posting transaction');
         }
-        console.log("In useEffect");
     }
 
     return (
@@ -61,9 +57,9 @@ export const TransactionScreen = () => {
                     {list}
                 </ListWrapper>
                 <InputWrapper>
-                    <InputButton onClick={handleType} transactionType={transactionType}>{transactionType ? '+' : '-'}</InputButton>
-                    <InputField onChange={handleAmount} type='number' min='0' placeholder='Transaction' />
-                    <Select onChange={handleFor}>
+                    <InputButton onClick={() => setTransactionType(!transactionType)} transactionType={transactionType}>{transactionType ? '+' : '-'}</InputButton>
+                    <InputField onChange={(e) => setAmount(e.target.value)} type='number' min='0' placeholder='Transaction' />
+                    <Select onChange={(e) => setAmountFor(e.target.value)}>
                         <option value='food'>Food</option>
                         <option value='earning'>Earnings</option>
                         <option value='transport'>Transport</option>
